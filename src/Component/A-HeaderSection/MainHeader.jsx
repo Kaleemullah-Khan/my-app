b8a060ee1421a7f661fa0d46c331cfb5ef18f885
Kaleemullah-Khan <kaleempathan567@gmail.com>
2022-01-10 17:56:40 -0800

    react css bootstrap

---
+++ src/Component/A-HeaderSection/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function MainHeader() {
+    const myStyle = {
+        color:"white",
+        textAlign:"center",
+        backgroundColor:"dodgerBlue",
+        padding:"10px"
+
+    }
+    return (
+        <div>
+            <h1 style={myStyle}>This Is A Navigation Bar</h1>
+        </div>
+    )
+}
---
+++ src/Component/HomePage/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainHeader from '../A-HeaderSection/MainHeader'
+import Navigation from '../B-Navigation/Navigation'
+export default function Landing() {
+    return (
+        <div>
+            <MainHeader/>
+            <Navigation/>
+        </div>
+    )
+}
+
